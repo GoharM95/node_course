461c0327f6e8ccfe42ab6e80f734df0afaba5a13
Gohar <gohar.mamunts@gmail.com>
2022-07-10 20:10:50 +0400

    adds logic of separating/sharing code between modules

---
+++ module_1/names.js
@@ -0,0 +1,8 @@
+// local - I don't want share this one
+const secret = "top secret";
+
+// shared, use module.exports to share
+const john = "john";
+const peter = "peter";
+
+module.exports = { john, peter };
