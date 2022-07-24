26269166586bfc5ff1c145b8345bfe5983a4f9d6
Gohar <gohar.mamunts@gmail.com>
2022-07-24 19:21:03 +0400

    working with html, css, js

--- udemy/web-server/src/app.js
+++ udemy/web-server/src/app.js
@@ -3,13 +3,13 @@ const express = require("express");
 const app = express();
 
 // 1
-app.get("", (req, res) => {
-  res.send("Hello express!");
-});
+// app.get("", (req, res) => {
+//   res.send("Hello express!");
+// });
 
-app.get("/help", (req, res) => {
-  res.send("Help page");
-});
+// app.get("/help", (req, res) => {
+//   res.send("Help page");
+// });
 
 // app.get("/about", (req, res) => {
 //   res.send("About page");
@@ -24,38 +24,51 @@ app.get("/help", (req, res) => {
 // });
 
 // 2
-app.get("/weather", (req, res) => {
-  res.send("<h1>Weather</h1>");
-});
+// app.get("/weather", (req, res) => {
+//   res.send("<h1>Weather</h1>");
+// });
 
-app.get("/about", (req, res) => {
-  res.send({ name: "Gohar", age: 27 });
-  // the object will be automatically stringified,
-  // an array will work the same way
-});
+// app.get("/about", (req, res) => {
+//   res.send({ name: "Gohar", age: 27 });
+//   // the object will be automatically stringified,
+//   // an array will work the same way
+// });
 
-app.get("/header", (req, res) => {
-  res.send("<h1>Header</h1>");
-});
+// app.get("/header", (req, res) => {
+//   res.send("<h1>Header</h1>");
+// });
 
-app.get("/weatherForecast", (req, res) => {
-  res.send({ forecast: "It's sunny", location: "Armenia" });
-});
+// app.get("/weatherForecast", (req, res) => {
+//   res.send({ forecast: "It's sunny", location: "Armenia" });
+// });
 
 // 3
+// const path = require("path");
+
+// const app2 = express();
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(path.join(__dirname, "../public"));
+
+// const publicDirectoryPath = path.join(__dirname, "../public");
+
+// // use() is used for customizing the server
+// app2.use(express.static(publicDirectoryPath));
+
+// app2.listen(3000, () => {
+//   console.log("Server is up on port 3000");
+// });
+
+// 4
 const path = require("path");
 
-const app2 = express();
-
-console.log(__dirname);
-console.log(__filename);
-console.log(path.join(__dirname, "../public"));
-
+const app3 = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
+console.log("__dirname", __dirname);
 
-// use() is used for customizing the server
-app2.use(express.static(publicDirectoryPath));
+app3.use(express.static(publicDirectoryPath));
 
-app2.listen(3000, () => {
+app3.listen(3000, () => {
   console.log("Server is up on port 3000");
 });
